fa6c03fb4fecc6ce2416d6cc6b569a1ce3ce299f
kamemarotamo <kamemarotamo@yahoo.co.jp>
2024-04-16 23:55:02 +0900

    がぞう

---
+++ src/app/firebase_criant.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXGe_UwAtV4Zzw8tGGFRmCCo9vQaLOp-c",
+    authDomain: "myon-cbc85.firebaseapp.com",
+    projectId: "myon-cbc85",
+    storageBucket: "myon-cbc85.appspot.com",
+    messagingSenderId: "255490106894",
+    appId: "1:255490106894:web:fe6a265679531d31b6fe5f",
+    measurementId: "G-86LK6S8YGX"
+  };
+
+  // Initialize Firebase
+
+//const analytics = getAnalytics(app);
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestorage = getStorage(app);
+const db = getFirestore(app);
+export default db;
--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,9 +1,37 @@
 'use client'
+import { useState} from "react"
+import { ref , getDownloadURL } from "firebase/storage";
+import { firestorage } from "../firebase_criant.js"
 
 export default function Mainpage() {
-    return (
-        <>
-            ログインしました。
-        </>
-    )
+  let [Image, setImage] = useState();
+  let url;
+  const gsReference = ref(
+    firestorage,
+    "gs://myon-cbc85.appspot.com/おたまとおたち.jpg"
+  );
+  getDownloadURL(gsReference)
+  .then((url) => {
+    setImage(url);
+  })
+  .catch((err) => console.log(err));     
+console.log(url)
+
+  return (
+      <>
+        左：おたま（ぬいぐるみ）
+        <br></br>
+        アニーのトゥモローをよく歌う。たまらぶゆー。
+        <br></br>
+        中：おたま（オタマトーン）
+        <br></br>
+        つけっぱなしにするとすぐに電池切れになる。基本無言でぱくぱくしている。
+        <br></br>
+        右：おたち
+        <br></br>
+        お立ちのお客様へのご案内が仕事。座りたい。
+        <br></br>
+        <img src={Image} alt="おたまとおたち" />
+      </>
+  )
 }
